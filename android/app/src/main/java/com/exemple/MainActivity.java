f780cc5aaaf58466458680b70eac92b34f734b96
PT Generasi Multi Indonusa <PTGMI@GMI-MacPro-Admin.local>
2021-03-23 17:26:30 +0700

    Final Build Android and iOS

--- android/app/src/main/java/com/exemple/MainActivity.java
+++ android/app/src/main/java/com/exemple/MainActivity.java
@@ -1,4 +1,4 @@
-package com.onlinevidcall;
+package com.exemple;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "onlineVidcall";
+    return "Exemple";
   }
 }
